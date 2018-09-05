46e876f72e1fb978af7b24b8510353af6167b22e
Alejandro Medina <5almero5@gmail.com>
2018-09-04 20:53:32 -0500

    Add MQTT comunication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
+import {Mqtt} from './modules/mqtt';
 import logo from './logo.svg';
 import './App.css';
 import BigCard from './Components/BigCard';
 import TinyCard from './Components/TinyCard';
+// REACT_APP_MQTT_IP=107.170.231.29 REACT_APP_MQTT_PORT=9001 npm run build
+const STATION_DEV_EUI = '1245';
 
 class App extends Component {
+  componentDidMount() {
+    const mqttClient = new Mqtt();
+    // example subs
+      mqttClient.subscribe(`application/2/device/${STATION_DEV_EUI}/rx`);
+    //
+    mqttClient.on('message', this.handleMessages);
+    mqttClient.on('close', this.handlerDisconnection);
+    this.setState({mqttClient});
+  }
+  handlerDisconnection = () => {
+    console.error('Disconection of MQTT Refres page :,(');
+  }
+
+handleMessages = (topic, message) => {
+    console.log(topic);
+    console.log(message);
+    const data = JSON.parse(message.toString());
+    console.log('DATA', data);
+
+    // Do something with the message
+    // :)
+  }
   render() {
     return (
       <div className="App">
